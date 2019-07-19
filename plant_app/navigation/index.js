7971b1e53be562c2b50124c5b8c89008ea89c92b
Dicu Razvan Gabriel <razvan.dicu@itsix.com>
2019-07-19 19:12:05 +0300

    added the project

---
+++ plant_app/navigation/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { createAppContainer, createStackNavigator } from "react-navigation";
+
+import Welcome from '../screens/Welcome';
+import Browse from '../screens/Browse';
+import Explore from '../screens/Explore';
+import Login from '../screens/Login';
+import Settings from '../screens/Settings';
+import Product from '../screens/Product';
+
+import { theme } from '../constants';
+
+const screens = createAppContainer({
+    Welcome,
+    Browse,
+    Explore, 
+    Login,
+    Settings,
+    Product
+}, {
+    defaultNavigationOptions: {
+        headerStyle: {},
+        headerBackImage: <Image />,
+        headerBackTitle: null,
+    }
+});
